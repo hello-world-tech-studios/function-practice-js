723e2008cae6ddb5819ed30575c1479a42693244
Sarah Betack <msbetack@gmail.com>
2017-06-13 10:37:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Functions, Functions, Functions!!
+
+## In `index.html`
+* Open `index.html` in Sublime
+* Open `index.html` in Chrome
+* Write a paragraph tag saying, "I LOVE FUNCTIONS!"
+* Create a link tag to this url https://media2.giphy.com/media/13HgwGsXF0aiGY/giphy.gif . This will be you during this exercise. A true hacker never stops for anything. Just kidding, I hope you would stop to put out a fire.
+
+## In `script.js`
+* Open the developer tools console in Chrome.
+### Use the directions below to write your functions in `script.js`
+###### Check each function as you go!
+* Write a function called `summertime`. It should return the phrase "It's summertime baby!"
+* Write a function called `addFive`. It should take in a number and return that number plus five.
+* Write a function called `double`. It should take in a number and return that number doubled.
+* Write a function called greetMe. It should take in someone's name and return "Hello, [The person's name]!". For example, greetMe("Joe") should return "Hello, Joe!"
+* `isItLessThanTen`: Should take in a number. If the given number is 100 or less it should return "Your number is less than 100". If the given number is greater than 100 is should return "Your number is not less than 100".
+* `inHighSchool`: Should take in a person's age. If they are old enough to be in high school it should return "High Schooler", otherwise it should return "Nope".
+* `areTheySpeeding`: Should take in a speed (in mph). If the speed is less the 75 it should return "Thanks for driving safely!", otherwise it should return "SLOW DOWN!!"
+* `addTwoNumbers`: Should take in TWO numbers and return the sum of those two numbers.
+* `multiplyTwoNumbers`: Should take in TWO numbers and return the product of those two numbers.
---
+++ script.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  // Write the `summertime` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(summertime())
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `addFive` below. 
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(addFive(10))
+//////////////////////////////////////////////////////////////////////////
+  // Write `double` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(double(6));
+//////////////////////////////////////////////////////////////////////////
+  // Write `greetMe` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(greetMe("Carl"));
+//////////////////////////////////////////////////////////////////////////
+  // Write `isItLessThanTen` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(isItLessThanTen(3));
+// console.log(isItLessThanTen(20));
+//////////////////////////////////////////////////////////////////////////
+  // Write `inHighSchool` below.
+
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(inHighSchool(17));
+  // console.log(inHighSchool(10));
+//////////////////////////////////////////////////////////////////////////
+  // Write `areTheySpeeding` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(areTheySpeeding(60));
+  // console.log(areTheySpeeding(90));
+//////////////////////////////////////////////////////////////////////////
+  // Write `addTwoNumbers` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(addTwoNumbers(3, 4));
+//////////////////////////////////////////////////////////////////////////
+  // Write `multiplyTwoNumbers` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(multiplyTwoNumbers(8, 10));
+//////////////////////////////////////////////////////////////////////////
+})
