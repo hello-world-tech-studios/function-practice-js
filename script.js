9315029525078f2761d367d26e87ab835963180a
Hack Reactor Student <student@hackreactor.com>
2017-06-20 14:30:59 -0500

    Script JS If else and more

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
 //////////////////////////////////////////////////////////////////////////
   // Write the `summertime` function below.
+function summertime(){
+  return "It's summertime baby!"
+}
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(summertime())
+  console.log(summertime())
 
 //////////////////////////////////////////////////////////////////////////
   // Write `addFive` below. 
-
+function addFive(number){
+  return number+5;
+}
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(addFive(10))
+  console.log(addFive(10));
 //////////////////////////////////////////////////////////////////////////
   // Write `double` below.
-
+function double(number){
+  return number*2;
+}
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(double(6));
+  console.log(double(6));
 //////////////////////////////////////////////////////////////////////////
   // Write `greetMe` below.
-
+function greetMe(name){
+  return "Hello "+name;
+}
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(greetMe("Carl"));
+  console.log(greetMe("Carl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
-
+function isItLessThanTen(number){
+  return number<10
+}
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(isItLessThanTen(3));
-// console.log(isItLessThanTen(20));
+  console.log(isItLessThanTen(3));
+console.log(isItLessThanTen(20));
 //////////////////////////////////////////////////////////////////////////
   // Write `inHighSchool` below.
-
-
-
-
-
+function inHighSchool(age){
+  if (age>=14){
+    return "Highschooler"  
+  } else {
+    return "Nope"
+  }
+}
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(inHighSchool(17));
-  // console.log(inHighSchool(10));
+  console.log(inHighSchool(17));
+  console.log(inHighSchool(10));
 //////////////////////////////////////////////////////////////////////////
   // Write `areTheySpeeding` below.
+
 
 
 
