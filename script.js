31186d314082ef2830270d511f892f8008bc6d83
Hack Reactor Student <student@hackreactor.com>
2017-06-20 15:16:34 -0500

    More practice with functions

--- script.js
+++ script.js
@@ -38,47 +38,71 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log(greetMe("Carl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
+  function isItLessThanTen(number) {
+    if (number > 10){
+      return ("Your number is not less than 10")
+    }
+    if (number < 10){
+      return ("Your number is less than 10") 
+    }
+   }
+
+
 
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(isItLessThanTen(3));
-// console.log(isItLessThanTen(20));
+  console.log(isItLessThanTen(3));
+console.log(isItLessThanTen(20));
 //////////////////////////////////////////////////////////////////////////
   // Write `inHighSchool` below.
-
-
-
-
+  function inHighSchool(number) {
+    if (number > 14 && number < 18) {
+      return ("High Schoolar")
+    } else {
+      return ("Nope")
+    }
+  }
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(inHighSchool(17));
-  // console.log(inHighSchool(10));
+  console.log(inHighSchool(17));
+  console.log(inHighSchool(10));
 //////////////////////////////////////////////////////////////////////////
   // Write `areTheySpeeding` below.
-
+  function areTheySpeeding(number) {
+    if (number > 75) {
+      return ("Slow Down!")
+    } else {
+        return ("Thanks for driving safely")
+    }
+  }
+    
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(areTheySpeeding(60));
-  // console.log(areTheySpeeding(90));
+  console.log(areTheySpeeding(60));
+  console.log(areTheySpeeding(90));
+  
 //////////////////////////////////////////////////////////////////////////
   // Write `addTwoNumbers` below.
-
-
+  function addTwoNumbers(number1, number2) {
+    return number1 + number2     
+}
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(addTwoNumbers(3, 4));
+  console.log(addTwoNumbers(3, 4));
 //////////////////////////////////////////////////////////////////////////
   // Write `multiplyTwoNumbers` below.
-
+  function multiplyTwoNumbers(number1, number2) {
+    return number1 * number2
+  }
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(multiplyTwoNumbers(8, 10));
+  console.log(multiplyTwoNumbers(8, 10));
 //////////////////////////////////////////////////////////////////////////
 })
