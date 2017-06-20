b0e4ebed8bb3e5f8ed213248686bb284bc109ae6
Hack Reactor Student <student@hackreactor.com>
2017-06-20 14:30:35 -0500

    functions

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
 //////////////////////////////////////////////////////////////////////////
   // Write the `summertime` function below.
-
+  function summertime(){
+    return "it's summertime baby!";
+}
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(summertime())
+  console.log(summertime())
 
 //////////////////////////////////////////////////////////////////////////
   // Write `addFive` below. 
-
+  function addFive(number){
+    return number +5;
+  }
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(addFive(10))
+  console.log(addFive(10))
 //////////////////////////////////////////////////////////////////////////
   // Write `double` below.
-
+  function double(number){
+    return number *2;
+  }
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(double(6));
+  console.log(double(6));
 //////////////////////////////////////////////////////////////////////////
   // Write `greetMe` below.
+  function greetMe(name){
+    return "Hello," + name + "!"
+  }
 
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(greetMe("Carl"));
+  console.log(greetMe("Carl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
+    // body...
+  function isItLessThanTen(number){
+    if(number > 10){
+      return"Your number is greater than 10."
+    }else(number < 10){
+      return "Your number is less than 10."
+    }
+  }
 
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(isItLessThanTen(3));
-// console.log(isItLessThanTen(20));
+  console.log(isItLessThanTen(3));
+  console.log(isItLessThanTen(20));
 //////////////////////////////////////////////////////////////////////////
   // Write `inHighSchool` below.
 
