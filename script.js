cf78dbe3a0674b830ad0bbecbd3f97d95824c84b
Hack Reactor Student <student@hackreactor.com>
2017-06-20 14:30:12 -0500

    practicing functions

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
 //////////////////////////////////////////////////////////////////////////
   // Write the `summertime` function below.
-
+  function summertime(){
+    return "It's summertime baby!"
+  }
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
   // console.log(summertime())
-
+  console.log(summertime())
 //////////////////////////////////////////////////////////////////////////
   // Write `addFive` below. 
-
-
-
+  function addFive(number){
+    return number +5
+  }
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(addFive(10))
+  console.log(addFive(10))
 //////////////////////////////////////////////////////////////////////////
   // Write `double` below.
+  function double(number) {
+    return number *2
+  }
+
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(double(6));
+  console.log(double(6));
 //////////////////////////////////////////////////////////////////////////
   // Write `greetMe` below.
-
-
-
+  function greetMe(name) {
+    return "Hello" +name
+  }
+  
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(greetMe("Carl"));
+  console.log(greetMe("Carl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
 
