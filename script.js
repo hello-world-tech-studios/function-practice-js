31930f1e51cebfb55e5ec9badc8b7887b44d932e
Hack Reactor Student <student@hackreactor.com>
2017-06-21 11:22:19 -0500

    FUN-CTIONS

--- script.js
+++ script.js
@@ -1,77 +1,118 @@
 document.addEventListener('DOMContentLoaded', function() {
 //////////////////////////////////////////////////////////////////////////
   // Write the `summertime` function below.
-
+function summertime() {
+return "It's summertime!"
+}
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(summertime())
+ console.log(summertime())
+
+ 
+
+
 
 //////////////////////////////////////////////////////////////////////////
   // Write `addFive` below. 
+function addFive(number) {
 
+
+return number + 5
+}
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(addFive(10))
+console.log(addFive(10))
 //////////////////////////////////////////////////////////////////////////
   // Write `double` below.
+function double(timesTwo) {
 
-
+return timesTwo * 2;
+}
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(double(6));
+   console.log(double(6));
 //////////////////////////////////////////////////////////////////////////
   // Write `greetMe` below.
+function greetMe(name) {
 
+return ("Hello " + name)
+}
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(greetMe("Carl"));
+   console.log(greetMe("CArl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
+function isItLessThanTen(lessThanTen) {
+if  (lessThanTen < 9) {
+return (lessThanTen + " is not less than ten")
+} else {
 
-
-
+return (lessThanTen + " Is more than ten")
+}
+}
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(isItLessThanTen(3));
-// console.log(isItLessThanTen(20));
+   console.log(isItLessThanTen(20));
+ console.log(isItLessThanTen(20));
 //////////////////////////////////////////////////////////////////////////
   // Write `inHighSchool` below.
+function inHighSchool(ageHigh) {
+if (ageHigh > 14) {
+return "Welcome!"
 
-
+} else {
+return "your not in Highschool"
+}
+ }
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(inHighSchool(17));
-  // console.log(inHighSchool(10));
+  console.log(inHighSchool(17));
+   console.log(inHighSchool(10));
 //////////////////////////////////////////////////////////////////////////
   // Write `areTheySpeeding` below.
+function areTheySpeeding(speedToSpeed) {
+if (speedToSpeed > 60) {
+return "SPPPEEEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDER"
 
+} else {
+return "Good job "
+}
+ }
 
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
-  // console.log(areTheySpeeding(60));
-  // console.log(areTheySpeeding(90));
+   console.log(areTheySpeeding(60));
+   console.log(areTheySpeeding(90));
 //////////////////////////////////////////////////////////////////////////
   // Write `addTwoNumbers` below.
+function addTwoNumbers(num1, num2) {
 
+return num1 + num2
+
+}
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(addTwoNumbers(3, 4));
+   console.log(addTwoNumbers(3, 4));
 //////////////////////////////////////////////////////////////////////////
   // Write `multiplyTwoNumbers` below.
+function multiplyTwoNumbers(num1M, num2M) {
 
+return num1M * num2M
+
+}
 
 
 
   // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(multiplyTwoNumbers(8, 10));
+   console.log(multiplyTwoNumbers(8, 10));
 //////////////////////////////////////////////////////////////////////////
 })
